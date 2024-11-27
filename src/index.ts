6f12435e9b564646c15226664b229cd527e2b5de
jagadeesh m <jagadeeshm2002@gmail.com>
2024-11-28 01:49:01 +0530

    changes for vercel build

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
-import express, { Express } from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db";
-import mongoose from "mongoose";
-import routes from "./routes";
-import { corsOptions } from "./config/corsOptions";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import serverless from 'serverless-http';
+import app from './app'; // Assumes your main app logic is in app.ts
 
-dotenv.config();
-connectDB();
-const app: Express = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
-const port = process.env.SERVER_PORT || 3000;
+// Create a serverless handler
+const handler = serverless(app);
 
-app.use("/api", routes);
-app.use("/", (req, res) => res.send("Hello World!"));
-
-
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(port, () => {
-    console.log("Server is running");
-  });
-});
-mongoose.connection.once("error", () => {
-  console.log("Error connecting to MongoDB");
-});
+export default async (req: any, res: any) => {
+  await handler(req, res);
+};
